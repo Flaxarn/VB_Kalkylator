7c4a5d7e685e1706ce5ea7ab1eaa047a50bd49e4
Kim Ståhl <flaxarn85@gmail.com>
2021-11-03 13:12:43 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,279 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Btn7 = New System.Windows.Forms.Button()
+        Me.Btn8 = New System.Windows.Forms.Button()
+        Me.Btn9 = New System.Windows.Forms.Button()
+        Me.Btn4 = New System.Windows.Forms.Button()
+        Me.Btn5 = New System.Windows.Forms.Button()
+        Me.Btn6 = New System.Windows.Forms.Button()
+        Me.Btn1 = New System.Windows.Forms.Button()
+        Me.Btn2 = New System.Windows.Forms.Button()
+        Me.BtnDot = New System.Windows.Forms.Button()
+        Me.Btn0 = New System.Windows.Forms.Button()
+        Me.Btn3 = New System.Windows.Forms.Button()
+        Me.BtnChange = New System.Windows.Forms.Button()
+        Me.BtnAdd = New System.Windows.Forms.Button()
+        Me.BtnSub = New System.Windows.Forms.Button()
+        Me.BtnMultiplie = New System.Windows.Forms.Button()
+        Me.BtnC = New System.Windows.Forms.Button()
+        Me.BtnSum = New System.Windows.Forms.Button()
+        Me.BtnDivide = New System.Windows.Forms.Button()
+        Me.BtnAC = New System.Windows.Forms.Button()
+        Me.TxtInput = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Btn7
+        '
+        Me.Btn7.Location = New System.Drawing.Point(12, 58)
+        Me.Btn7.Name = "Btn7"
+        Me.Btn7.Size = New System.Drawing.Size(50, 50)
+        Me.Btn7.TabIndex = 0
+        Me.Btn7.Text = "7"
+        Me.Btn7.UseVisualStyleBackColor = True
+        '
+        'Btn8
+        '
+        Me.Btn8.Location = New System.Drawing.Point(68, 58)
+        Me.Btn8.Name = "Btn8"
+        Me.Btn8.Size = New System.Drawing.Size(50, 50)
+        Me.Btn8.TabIndex = 1
+        Me.Btn8.Text = "8"
+        Me.Btn8.UseVisualStyleBackColor = True
+        '
+        'Btn9
+        '
+        Me.Btn9.Location = New System.Drawing.Point(124, 58)
+        Me.Btn9.Name = "Btn9"
+        Me.Btn9.Size = New System.Drawing.Size(50, 50)
+        Me.Btn9.TabIndex = 2
+        Me.Btn9.Text = "9"
+        Me.Btn9.UseVisualStyleBackColor = True
+        '
+        'Btn4
+        '
+        Me.Btn4.Location = New System.Drawing.Point(12, 114)
+        Me.Btn4.Name = "Btn4"
+        Me.Btn4.Size = New System.Drawing.Size(50, 50)
+        Me.Btn4.TabIndex = 3
+        Me.Btn4.Text = "4"
+        Me.Btn4.UseVisualStyleBackColor = True
+        '
+        'Btn5
+        '
+        Me.Btn5.Location = New System.Drawing.Point(68, 114)
+        Me.Btn5.Name = "Btn5"
+        Me.Btn5.Size = New System.Drawing.Size(50, 50)
+        Me.Btn5.TabIndex = 4
+        Me.Btn5.Text = "5"
+        Me.Btn5.UseVisualStyleBackColor = True
+        '
+        'Btn6
+        '
+        Me.Btn6.Location = New System.Drawing.Point(124, 114)
+        Me.Btn6.Name = "Btn6"
+        Me.Btn6.Size = New System.Drawing.Size(50, 50)
+        Me.Btn6.TabIndex = 5
+        Me.Btn6.Text = "6"
+        Me.Btn6.UseVisualStyleBackColor = True
+        '
+        'Btn1
+        '
+        Me.Btn1.Location = New System.Drawing.Point(12, 170)
+        Me.Btn1.Name = "Btn1"
+        Me.Btn1.Size = New System.Drawing.Size(50, 50)
+        Me.Btn1.TabIndex = 6
+        Me.Btn1.Text = "1"
+        Me.Btn1.UseVisualStyleBackColor = True
+        '
+        'Btn2
+        '
+        Me.Btn2.Location = New System.Drawing.Point(68, 170)
+        Me.Btn2.Name = "Btn2"
+        Me.Btn2.Size = New System.Drawing.Size(50, 50)
+        Me.Btn2.TabIndex = 7
+        Me.Btn2.Text = "2"
+        Me.Btn2.UseVisualStyleBackColor = True
+        '
+        'BtnDot
+        '
+        Me.BtnDot.Location = New System.Drawing.Point(68, 239)
+        Me.BtnDot.Name = "BtnDot"
+        Me.BtnDot.Size = New System.Drawing.Size(50, 50)
+        Me.BtnDot.TabIndex = 8
+        Me.BtnDot.Text = "."
+        Me.BtnDot.UseVisualStyleBackColor = True
+        '
+        'Btn0
+        '
+        Me.Btn0.Location = New System.Drawing.Point(12, 239)
+        Me.Btn0.Name = "Btn0"
+        Me.Btn0.Size = New System.Drawing.Size(50, 50)
+        Me.Btn0.TabIndex = 9
+        Me.Btn0.Text = "0"
+        Me.Btn0.UseVisualStyleBackColor = True
+        '
+        'Btn3
+        '
+        Me.Btn3.Location = New System.Drawing.Point(124, 170)
+        Me.Btn3.Name = "Btn3"
+        Me.Btn3.Size = New System.Drawing.Size(50, 50)
+        Me.Btn3.TabIndex = 10
+        Me.Btn3.Text = "3"
+        Me.Btn3.UseVisualStyleBackColor = True
+        '
+        'BtnChange
+        '
+        Me.BtnChange.Location = New System.Drawing.Point(124, 239)
+        Me.BtnChange.Name = "BtnChange"
+        Me.BtnChange.Size = New System.Drawing.Size(50, 50)
+        Me.BtnChange.TabIndex = 11
+        Me.BtnChange.Text = "+/-"
+        Me.BtnChange.UseVisualStyleBackColor = True
+        '
+        'BtnAdd
+        '
+        Me.BtnAdd.Location = New System.Drawing.Point(206, 170)
+        Me.BtnAdd.Name = "BtnAdd"
+        Me.BtnAdd.Size = New System.Drawing.Size(50, 50)
+        Me.BtnAdd.TabIndex = 12
+        Me.BtnAdd.Text = "+"
+        Me.BtnAdd.UseVisualStyleBackColor = True
+        '
+        'BtnSub
+        '
+        Me.BtnSub.Location = New System.Drawing.Point(262, 170)
+        Me.BtnSub.Name = "BtnSub"
+        Me.BtnSub.Size = New System.Drawing.Size(50, 50)
+        Me.BtnSub.TabIndex = 13
+        Me.BtnSub.Text = "-"
+        Me.BtnSub.UseVisualStyleBackColor = True
+        '
+        'BtnMultiplie
+        '
+        Me.BtnMultiplie.Location = New System.Drawing.Point(262, 114)
+        Me.BtnMultiplie.Name = "BtnMultiplie"
+        Me.BtnMultiplie.Size = New System.Drawing.Size(50, 50)
+        Me.BtnMultiplie.TabIndex = 14
+        Me.BtnMultiplie.Text = "*"
+        Me.BtnMultiplie.UseVisualStyleBackColor = True
+        '
+        'BtnC
+        '
+        Me.BtnC.Location = New System.Drawing.Point(206, 58)
+        Me.BtnC.Name = "BtnC"
+        Me.BtnC.Size = New System.Drawing.Size(50, 50)
+        Me.BtnC.TabIndex = 15
+        Me.BtnC.Text = "C"
+        Me.BtnC.UseVisualStyleBackColor = True
+        '
+        'BtnSum
+        '
+        Me.BtnSum.Location = New System.Drawing.Point(206, 239)
+        Me.BtnSum.Name = "BtnSum"
+        Me.BtnSum.Size = New System.Drawing.Size(106, 50)
+        Me.BtnSum.TabIndex = 16
+        Me.BtnSum.Text = "="
+        Me.BtnSum.UseVisualStyleBackColor = True
+        '
+        'BtnDivide
+        '
+        Me.BtnDivide.Location = New System.Drawing.Point(206, 114)
+        Me.BtnDivide.Name = "BtnDivide"
+        Me.BtnDivide.Size = New System.Drawing.Size(50, 50)
+        Me.BtnDivide.TabIndex = 17
+        Me.BtnDivide.Text = "/"
+        Me.BtnDivide.UseVisualStyleBackColor = True
+        '
+        'BtnAC
+        '
+        Me.BtnAC.Location = New System.Drawing.Point(262, 58)
+        Me.BtnAC.Name = "BtnAC"
+        Me.BtnAC.Size = New System.Drawing.Size(50, 50)
+        Me.BtnAC.TabIndex = 18
+        Me.BtnAC.Text = "AC"
+        Me.BtnAC.UseVisualStyleBackColor = True
+        '
+        'TxtInput
+        '
+        Me.TxtInput.Location = New System.Drawing.Point(12, 12)
+        Me.TxtInput.Name = "TxtInput"
+        Me.TxtInput.Size = New System.Drawing.Size(300, 31)
+        Me.TxtInput.TabIndex = 19
+        '
+        'Form1
+        '
+        Me.AcceptButton = Me.BtnSum
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(333, 312)
+        Me.Controls.Add(Me.TxtInput)
+        Me.Controls.Add(Me.BtnAC)
+        Me.Controls.Add(Me.BtnDivide)
+        Me.Controls.Add(Me.BtnSum)
+        Me.Controls.Add(Me.BtnC)
+        Me.Controls.Add(Me.BtnMultiplie)
+        Me.Controls.Add(Me.BtnSub)
+        Me.Controls.Add(Me.BtnAdd)
+        Me.Controls.Add(Me.BtnChange)
+        Me.Controls.Add(Me.Btn3)
+        Me.Controls.Add(Me.Btn0)
+        Me.Controls.Add(Me.BtnDot)
+        Me.Controls.Add(Me.Btn2)
+        Me.Controls.Add(Me.Btn1)
+        Me.Controls.Add(Me.Btn6)
+        Me.Controls.Add(Me.Btn5)
+        Me.Controls.Add(Me.Btn4)
+        Me.Controls.Add(Me.Btn9)
+        Me.Controls.Add(Me.Btn8)
+        Me.Controls.Add(Me.Btn7)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Btn7 As Button
+    Friend WithEvents Btn8 As Button
+    Friend WithEvents Btn9 As Button
+    Friend WithEvents Btn4 As Button
+    Friend WithEvents Btn5 As Button
+    Friend WithEvents Btn6 As Button
+    Friend WithEvents Btn1 As Button
+    Friend WithEvents Btn2 As Button
+    Friend WithEvents BtnDot As Button
+    Friend WithEvents Btn0 As Button
+    Friend WithEvents Btn3 As Button
+    Friend WithEvents BtnChange As Button
+    Friend WithEvents BtnAdd As Button
+    Friend WithEvents BtnSub As Button
+    Friend WithEvents BtnMultiplie As Button
+    Friend WithEvents BtnC As Button
+    Friend WithEvents BtnSum As Button
+    Friend WithEvents BtnDivide As Button
+    Friend WithEvents BtnAC As Button
+    Friend WithEvents TxtInput As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,172 @@
+﻿Public Class Form1
+    'Globala variabler
+    Dim tal1 As Single ' tal 1
+    Dim tal2 As Single ' tal 2
+    Dim resultat As Single ' resultat
+
+    Dim [operator] As Integer ' val av räknesätt för case
+    Dim negativt As Boolean ' för kolla av negativa nummer
+
+    Private Sub TxtInput_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TxtInput.KeyPress
+        'GÄLLER ENDAST INPUT MED TBORD
+        'Tillåta siffror, backspace och.
+        If (e.KeyChar < "0"c Or e.KeyChar > "9"c) And e.KeyChar <> vbBack And e.KeyChar <> "."c Then
+            e.Handled = True
+        End If
+
+        'Hantera extra . // OBS BLIR FEL MED FLERA NUMMER (ej tbord )
+        If e.KeyChar = "."c And InStr(sender.text, "."c) > 0 Then
+            e.Handled = True
+        End If
+
+        'Hantera - tecken // OBS BLIR FEL MED FLERA NUMMER ( ej tbord )
+        If e.KeyChar = "-"c Then
+            If sender.Text.Startswith("-"c) Then
+                sender.Text = Mid(sender.Text, 2)
+            Else
+                sender.text = "-" & sender.text
+            End If
+            sender.select(sender.text.length, 0)
+        End If
+
+    End Sub
+
+    Private Sub BtnAdd_Click(sender As Object, e As EventArgs) Handles BtnAdd.Click
+        'User slected the add button
+        [operator] = 1
+        'Convert into a number and transfer the value from
+        'The text box on the form into the first number
+
+        tal1 = Val(TxtInput.Text)
+
+
+        TxtInput.Text = ""
+    End Sub
+
+    Private Sub BtnSub_Click(sender As Object, e As EventArgs) Handles BtnSub.Click
+        [operator] = 2
+        'Convert into a number and transfer the value from
+        'The text box on the form into the first number
+        tal1 = Val(TxtInput.Text)
+        TxtInput.Text = ""
+    End Sub
+
+    Private Sub BtnDivide_Click(sender As Object, e As EventArgs) Handles BtnDivide.Click
+        'User slected the Divide button
+        [operator] = 4
+        'Convert into a number and transfer the value from
+        'The text box on the form into the first number
+        tal1 = Val(TxtInput.Text)
+
+        TxtInput.Text = ""
+    End Sub
+
+    Private Sub BtnMultiplie_Click(sender As Object, e As EventArgs) Handles BtnMultiplie.Click
+        'User slected the multiply button
+        [operator] = 3
+        'Convert into a number and transfer the value from
+        'The text box on the form into the first number
+        tal1 = Val(TxtInput.Text)
+
+        TxtInput.Text = ""
+    End Sub
+
+    Private Sub Btn1_Click(sender As Object, e As EventArgs) Handles Btn1.Click
+        TxtInput.Text = TxtInput.Text & "1"
+    End Sub
+
+    Private Sub Btn2_Click(sender As Object, e As EventArgs) Handles Btn2.Click
+        TxtInput.Text = TxtInput.Text & "2"
+    End Sub
+
+    Private Sub Btn3_Click(sender As Object, e As EventArgs) Handles Btn3.Click
+        TxtInput.Text = TxtInput.Text & "3"
+    End Sub
+
+    Private Sub Btn4_Click(sender As Object, e As EventArgs) Handles Btn4.Click
+        TxtInput.Text = TxtInput.Text & "4"
+    End Sub
+
+    Private Sub Btn5_Click(sender As Object, e As EventArgs) Handles Btn5.Click
+        TxtInput.Text = TxtInput.Text & "5"
+    End Sub
+
+    Private Sub Btn6_Click(sender As Object, e As EventArgs) Handles Btn6.Click
+        TxtInput.Text = TxtInput.Text & "6"
+    End Sub
+
+    Private Sub Btn7_Click(sender As Object, e As EventArgs) Handles Btn7.Click
+        TxtInput.Text = TxtInput.Text & "7"
+    End Sub
+
+    Private Sub Btn8_Click(sender As Object, e As EventArgs) Handles Btn8.Click
+        TxtInput.Text = TxtInput.Text & "8"
+    End Sub
+
+    Private Sub Btn9_Click(sender As Object, e As EventArgs) Handles Btn9.Click
+        TxtInput.Text = TxtInput.Text & "9"
+    End Sub
+
+    Private Sub Btn0_Click(sender As Object, e As EventArgs) Handles Btn0.Click
+        TxtInput.Text = TxtInput.Text & "0"
+    End Sub
+
+    Private Sub BtnDot_Click(sender As Object, e As EventArgs) Handles BtnDot.Click
+        If InStr(TxtInput.Text, "."c) = 0 Then
+            TxtInput.Text = TxtInput.Text & "."
+        End If
+
+        If InStr(TxtInput.Text, "."c) > 0 Then
+            ' här ska vi ta bort decimal om det redan finns en...
+
+        End If
+    End Sub
+
+    Private Sub BtnAC_Click(sender As Object, e As EventArgs) Handles BtnAC.Click
+        [operator] = 0
+        tal1 = 0
+        tal2 = 0
+        TxtInput.Text = ""
+        resultat = Nothing
+    End Sub
+
+    Private Sub BtnSum_Click(sender As Object, e As EventArgs) Handles BtnSum.Click
+        tal2 = Val(TxtInput.Text)
+
+        Select Case [operator]
+            Case Is = 1
+                resultat = tal1 + tal2
+            Case Is = 2
+                resultat = tal1 - tal2
+            Case Is = 3
+                resultat = tal1 * tal2
+            Case Is = 4
+                resultat = tal1 / tal2
+        End Select
+        TxtInput.Text = resultat
+    End Sub
+
+    Private Sub BtnChange_Click(sender As Object, e As EventArgs) Handles BtnChange.Click
+        Dim MINUSVALUE
+        'Sign state = false on load of form
+        If TxtInput.Text = "-" & TxtInput.Text Then
+            MsgBox("error start again")
+        End If
+        If negativt = False Then
+            TxtInput.Text = "-" & TxtInput.Text
+            negativt = True
+        Else
+            'SignState = True
+
+            MINUSVALUE = Val(TxtInput.Text)
+            'Value now positive
+            MINUSVALUE = Val("-1" * MINUSVALUE)
+            TxtInput.Text = MINUSVALUE
+            negativt = False
+        End If
+    End Sub
+
+    Private Sub BtnC_Click(sender As Object, e As EventArgs) Handles BtnC.Click
+        TxtInput.Text = ""
+    End Sub
+End Class
